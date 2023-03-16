4639fb51ba7da04cc32cfd31b7ca5ceb916c6080
Renan Ferreira Renzo <66973821+renan93-hub@users.noreply.github.com>
2023-03-16 14:11:04 -0300

    Add files via upload

---
+++ Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+
+export const livros = [
+    {nome: 'Liderança em Desing', id: 1, src:livro},
+    {nome: 'Java Script Assertivo', id: 2, src:livro},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro}
+]
+
---
+++ UltimosLancamentos/dadosUltimosLacamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Desing', id: 1, src:livro}, 
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro}
+]
